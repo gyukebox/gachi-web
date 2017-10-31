9b2750a64717234f762818e7bf10b6aa31983f08
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-10-31 15:40:23 +0900

    sample template without styling

--- website/gachi/views.py
+++ website/gachi/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return render(request, template_name='gachi/index.html')
+    sample_context = {
+        'proverbs': [
+            '이것은 명언 1. 시간에 관한 명언',
+            '이것은 명언 2. 성공에 관한 명언',
+            '이것은 명언 3. 부에 관한 명언',
+            '이것은 명언 4. 사랑에 관한 명언',
+            '이것은 명언 5. 친구에 관한 명언',
+            '이것은 명언 6. 배움에 관한 명언'
+        ],
+        'description': [
+            '한국어 - 이것은 예시입니다. 예시라고요',
+            'English - This is a description for sample template. Our website will be look like this',
+            'Español - Éste es el modelo de nuestra página web.',
+        ]
+    }
+    return render(request, template_name='gachi/index.html', context=sample_context)
