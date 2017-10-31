6f6a23a3aa0e1cdabf713f5602061a23c3ab80cd
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-11-01 01:41:53 +0900

    샘플 스타일링 완료!

--- website/gachi/views.py
+++ website/gachi/views.py
@@ -13,9 +13,16 @@ def index(request):
             '이것은 명언 6. 배움에 관한 명언'
         ],
         'description': [
-            '한국어 - 이것은 예시입니다. 예시라고요',
-            'English - This is a description for sample template. Our website will be look like this',
-            'Español - Éste es el modelo de nuestra página web.',
+            '한국어 - 이것은 예시입니다. 예시라고요. 예시란 말이다 이사람들아 어떤 일반적 진술에 대해서 '
+            '그에 관련된 특수한 진술을 미리 들어 보이는 것. 예를 들어 "감정에 대한 이해는 나이가 들수록 복잡해진다. '
+            '예를 들어 시원섭섭함이라는 특수한 감정은 어느 정도 성인이 되어서야 실감할 수 있다." '
+            '와 같은 방식의 서술기법을 활용하는 것이다.',
+            'English - This is a description for sample template. Our website will be look like this. '
+            'one of a number of things, or a part of something, taken to show the character of the whole:',
+            'Español - Éste es el modelo de nuestra página web. '
+            'Un ejemplo sirve para explicar o ilustrar una afirmación general, '
+            'o para proporcionar un caso particular que hace de modelo para el caso general. '
+            'El ejemplo es escogido libremente, pero busca aclarar la comprensión de un fenómeno o proceso dado.',
         ]
     }
     return render(request, template_name='gachi/index.html', context=sample_context)
