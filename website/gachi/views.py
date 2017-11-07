1bc6581e2e37418957587624294c593ce02e78c1
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-11-08 03:29:04 +0900

    데이터 분석모델, 워드 클라우드 생성모델 구현

--- website/gachi/views.py
+++ website/gachi/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import json
-import numpy as np
-
+from collections import Counter
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
@@ -18,19 +16,21 @@ def index(request):
     :return: Rendered template, named index.html
     """
 
+    import json
+    import numpy as np
+
     sample_context = {
         'proverbs': [
 
         ],
         'description': [
-            '함께 찾는 우리의 가치: 당신의 삶에서 가장 중요한 것은 무엇입니까?',
             '여러분들께 몇 가지의 문구를 보여 드릴 겁니다. 보시고 가장 마음에 드시는 문구 하나만 골라 주세요!'
         ]
     }
 
     try:
-        json_file = open('/static/gachi/config.json', 'r', encoding='utf8')
-        json_object = json.load(json)
+        json_file = open('gachi/static/gachi/config.json', 'r', encoding='utf8')
+        json_object = json.load(json_file)
 
         for i in range(6):
             start_index = json_object["Proverbs"][i]["Start"]
@@ -46,7 +46,7 @@ def index(request):
             start_index = indices[i]
             end_index = indices[i + 1]
 
-            key = np.random.randint(low=start_index, high=end_index + 1)
+            key = np.random.randint(low=start_index, high=end_index)
             sample_context['proverbs'].append(Proverb.objects.get(pk=key).text)
 
     return render(request, template_name='gachi/index.html', context=sample_context)
@@ -87,31 +87,100 @@ def analyze(request):
     """
     View function for analyzing data
     URL: host/analysis
-    :param request:
-    :return:
+    :param request: Http request
+    :return: Template rendered, by name - result.html
     """
     people = Person.objects.all()
     people = sorted(people, key=lambda x: x.age)
 
-    # test
-    for person in people:
-        print('=' * 50)
-        print(person.find_category())
-        print(person.age)
-        print(person.gender)
+    generate_worldloud([person.selected_proverb for person in people])
+
+    data = [
+        {
+            'category': person.find_category(),
+            'age': person.age,
+            'gender': person.gender
+        }
+        for person in people
+    ]
 
     # make analysis result into json-like dict
     analysis_result = dict()
+    analysis_result['age'] = analyze_by_age(data)
+    analysis_result['gender'] = analyze_by_gender(data)
 
-    # TODO analysis by age
-    # TODO analysis by gender
+    analysis_result['description'] = [
+        '소중한 시간을 내어 주셔서 설문조사에 응해 주셔서 감사합니다!',
+        '여러분들의 응답을 연령별, 그리고 성별별로 분석해 보았습니다. 확인해보시죠!'
+    ]
 
     return render(request, template_name='gachi/result.html', context=analysis_result)
 
 
 def analyze_by_age(data):
-    pass
+    """
+    Sub-function for analysis view.
+    Analyzes data by age.
+    :param data: Data to analyze
+    :return: Analysis, by json format
+    """
+    pre = [datum['category'] for datum in data if datum['age'] < 23]
+    mid = [datum['category'] for datum in data if 23 <= datum['age'] < 27]
+    post = [datum['category'] for datum in data if datum['age'] >= 27]
+
+    pre_counter = Counter(pre)
+    mid_counter = Counter(mid)
+    post_counter = Counter(post)
+
+    final_data = {
+        'pre': sorted(pre_counter.items(), key=lambda x: x[1], reverse=True),
+        'mid': sorted(mid_counter.items(), key=lambda x: x[1], reverse=True),
+        'post': sorted(post_counter.items(), key=lambda x: x[1], reverse=True),
+    }
+    return final_data
 
 
 def analyze_by_gender(data):
-    pass
+    """
+    Sub-function for analysis view.
+    Analyzes data by gender.
+    :param data: Data to analyze
+    :return: Analysis, by json format
+    """
+    male = []
+    female = []
+    for person in data:
+        if person['gender'] == 'M':
+            male.append(person)
+        else:
+            female.append(person)
+
+    male_counter = Counter([data['category'] for data in male])
+    female_counter = Counter([data['category'] for data in female])
+
+    final_data = {
+        'male': sorted(male_counter.items(), key=lambda x: x[1], reverse=True),
+        'female': sorted(female_counter.items(), key=lambda x: x[1], reverse=True),
+    }
+    return final_data
+
+
+def generate_worldloud(text):
+    from wordcloud import WordCloud
+    from konlpy.tag import Twitter
+
+    print(text)
+    phrases = ''
+    for phrase in text:
+        phrases += phrase + ' '
+
+    twitter = Twitter()
+    nouns = twitter.nouns(phrases)
+    words = ''
+    for word in nouns:
+        words += word + ' '
+
+    wordcloud = WordCloud(font_path='/Library/fonts/AppleGothic.ttf', background_color='white', width=600, height=400)
+    wordcloud.generate_from_text(words)
+    wordcloud.to_file('gachi/static/gachi/images/wordcloud.png')
+
